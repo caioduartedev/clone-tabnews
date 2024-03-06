e0c96825e2d707ae67c4ccb11fef3addeb0ed33b
Caio Duarte <53125467+caioduartedev@users.noreply.github.com>
2024-03-06 20:12:02 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Hey, if you like code, dá uma risadinha. =)</h1>
+  return <h1>Hey, if you like code, dá uma risadinha. =)</h1>;
+}
+
+function teste() {
+  return;
 }
 
 export default Home;
